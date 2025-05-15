579cd0c63c49f9fa72c53d64cb6e3973c5e6ab42
TORASKAR Manish (ADM) <manish.toraskar-adm@kering.com>
2025-05-15 11:18:12 +0200

    updated code

---
+++ terraform/IAM.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_policy" "deny_ec2_public_ip" {
+  name        = "DenyEC2LaunchWithPublicIP"
+  description = "Deny launching EC2 instances with public IP"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "DenyEC2LaunchWithPublicIP",
+        Effect = "Deny",
+        Action = "ec2:RunInstances",
+        Resource = "arn:aws:ec2:*:*:instance/*",
+        Condition = {
+          Bool = {
+            "ec2:AssociatePublicIpAddress" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+#3resource "aws_iam_role_policy_attachment" "attach_deny_policy_to_role" {
+#  role       = aws_iam_role.example.name
+#  policy_arn = aws_iam_policy.deny_ec2_public_ip.arn
+#}
+
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "state-mt"
+    bucket         = "atlantis-state-mt"
     key            = "prod/ec2/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
